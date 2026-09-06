ec7a34bcfe54c67a0852c366966955eba424d6dc
Franricoc <franricoc@gmail.com>
2026-09-06 17:05:14 -0600

    Publicación de versión v1.7.230.2 (Universal Release)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.streamflixreborn.streamflix"
         minSdk = 21
         targetSdk = 35
-        versionCode = 164
+        versionCode = 165
         versionName = "1.7.230.2"
 
         buildConfigField("String", "APP_LAYOUT", "\"${properties.getProperty("APP_LAYOUT") ?: "universal"}\"")
